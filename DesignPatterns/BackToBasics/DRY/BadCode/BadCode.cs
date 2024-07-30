1323a2fd57334c8de666ead91d1dca685a620cf2
Laura <Laura@DESKTOP-3E6HHQ5>
2024-07-30 20:51:52 +0100

    Adding DRY examples

---
+++ DesignPatterns/BackToBasics/DRY/BadCode/BadCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BackToBasics.DRY
+{
+    public class BadCode
+    {
+        public double CalculateBill(List<double> prices)
+        {
+            var totalPrice = prices.Sum();
+            var tax = totalPrice * 0.2;
+            return totalPrice + tax;
+        }
+
+        public double CalculateWages(double hourlyRate, int hoursWorked)
+        {
+            var totalWage = hourlyRate * hoursWorked;
+            var tax = totalWage * 0.2;
+            return totalWage - tax;
+        }
+    }
+}
---
+++ DesignPatterns/BackToBasics/DRY/GoodCode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BackToBasics.DRY
+{
+    public class BillService
+    {
+        TaxHelper _taxHelper = new TaxHelper();
+        public double CalculateBill(List<double> prices)
+        {
+            var totalPrice = prices.Sum();
+            var tax = _taxHelper.CalculateTax(totalPrice);
+            return totalPrice + tax;
+        }
+    }
+
+    public class WageService
+    {
+        TaxHelper _taxHelper = new TaxHelper();
+        public double CalculateWages(double hourlyRate, int hoursWorked)
+        {
+            var totalWage = hourlyRate * hoursWorked;
+            var tax = _taxHelper.CalculateTax(totalWage);
+            return totalWage - tax;
+        }
+    }
+
+    public class TaxHelper
+    {
+        public double CalculateTax(double amount)
+        {
+            return amount * 0.2;
+        }
+    }
+}
